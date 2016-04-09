edc740a8b9621611c4e24d18618dc34b55d8bada
Anonymous Candidate <anon@example.com>
2016-04-08 18:19:44 -0600

    better handler, uses ring response and header

--- todo-list/src/todo_list/hello_gif_handler.clj
+++ todo-list/src/todo_list/hello_gif_handler.clj
@@ -5,14 +5,9 @@
 )
 
 (defn handler [request-map]
- 	{ :status 200
-	  :headers {"Content-Type" "text/html"}
-	  :body (str "<html><body>\n"
-				"<img src=\"images/const_e0.gif\"/><br>"
-				"</body></html>")})
-
-;; needs a content type
-(defn handler-wtf [request-map]
-	;;(response/response
-	(ring.util.response/response
-		(str "<html><body>Heloooo</body></html>")))
+	(response/header 
+		(response/response
+			(str "<html><body>"
+				 "<img src=\"images/const_e0.gif\"/>"
+				 "</body></html>"))
+		"Content-Type" "text/html"))
